8b36b14fffc3a19ab7dd722d7e0bdde4fe034fb2
herald309 <heraldrobinson309@gmail.com>
2021-06-29 10:27:18 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder builder = new StringBuilder();
+            int middle = input.Length / 2;
+            for(int i= middle; i< input.Length; i++)
+            {
+                builder.Append(input[i]);
+                Console.WriteLine(builder.ToString());
+            }
+
+            int lastElement = input.Length - (middle +1);
+            for (int i = 0; i < lastElement; i++)
+            {
+                builder.Append(input[i]);
+                Console.WriteLine(builder.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
